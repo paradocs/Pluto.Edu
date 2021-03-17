d1404f26f9a6b8ad77a518ff770ba601e4d0c904
David Tan Creti <paradocs.home@gmail.com>
2021-03-17 06:08:29 -0500

    Update mathFunctionObjects.9.paradocs.demo.jl

--- mathFunctionObjects.9.paradocs.demo.jl
+++ mathFunctionObjects.9.paradocs.demo.jl
@@ -1,5 +1,5 @@
 ### A Pluto.jl notebook ###
-# v0.12.20
+# v0.12.21
 
 using Markdown
 using InteractiveUtils
@@ -7,21 +7,27 @@ using InteractiveUtils
 # ╔═╡ eb3b3be0-67d1-11eb-144b-2f9581950503
 md"""
 #### Math can be easy!
-#### Just write simple steps.
+#### When you come on someting new
+#### $~~~~$just think in simple steps.
 """
 
 # ╔═╡ 66294030-67d3-11eb-25b1-8d25703546db
 md"""
-#### sqrt
-##### ??? What is this?
-####  Pluto 🛡️ to the rescue!
-#####  Doing Things can be objects.
+#### sqrt()  
+##### ??? What is this?  
+####  Pluto 🛡️ to the rescue!  
+##### Doing Things have a name.$~~$ Here that is $~~ sqrt$
+##### The $~~$()$~~$ is a place for something to worked on.   
+##### Doing Things can be copied to a new name.
+##### We may like one,$~$ but both do the same job.
 """
 
 # ╔═╡ f4835c0a-67db-11eb-134c-85092b5a1ced
 md"""
 ##### Looking for a short
 ##### cut across a square 🔲?
+##### Walking on the edges is longer.
+##### But corner to corner is shortest,
 ##### We call it the root 🥕.
 """
 
@@ -35,12 +41,15 @@ md"""
 """
 
 
+# ╔═╡ fca3b432-86f8-11eb-3086-b3adfccd0cda
+sqrt(2)
+
 # ╔═╡ 84554b8a-67ce-11eb-2e10-7550cbde4485
 md"""
 ##### Hard to think of sqrt?
 ##### I bet you will never forget
 ## 🔲🥕
-##### Lets create a new Doing Thing.
+##### Let's use $~$=$~$ to make a new Doing Thing.
 """
 
 # ╔═╡ f8c8c828-67d0-11eb-16b6-f384a4a4a5df
@@ -49,7 +58,7 @@ md"""
 # ╔═╡ 279bdd4e-67e2-11eb-1adb-b98afc7e812e
 md"""
 ##### Yes, it works!
-##### Will you remember?
+##### Tomorrow, will you remember?
 ### 🔲🥕  $~~~~or~~~~~$   sqrt
 """
 
@@ -57,12 +66,20 @@ md"""
 🔲🥕(2)
 
 
+# ╔═╡ 7dab943e-86f9-11eb-284d-4fb84d4c9b63
+md"""
+##### Wow!$~~$ The long way is 2.
+##### But the short way is not even 1 and 1/2.
+"""
+
 # ╔═╡ Cell order:
 # ╠═eb3b3be0-67d1-11eb-144b-2f9581950503
 # ╠═66294030-67d3-11eb-25b1-8d25703546db
 # ╠═f4835c0a-67db-11eb-134c-85092b5a1ced
 # ╠═1976e31c-67df-11eb-2d4a-1d8e612a99a9
+# ╠═fca3b432-86f8-11eb-3086-b3adfccd0cda
 # ╠═84554b8a-67ce-11eb-2e10-7550cbde4485
 # ╠═f8c8c828-67d0-11eb-16b6-f384a4a4a5df
 # ╠═279bdd4e-67e2-11eb-1adb-b98afc7e812e
 # ╠═1c3a7e52-67d1-11eb-0c5a-a5b36b4fc989
+# ╠═7dab943e-86f9-11eb-284d-4fb84d4c9b63
